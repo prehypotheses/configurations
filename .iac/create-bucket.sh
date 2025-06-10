e657e7ccbfbd98db8d038b936e071735b92274e3
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-10 20:35:00 +0100

    Delete .iac/create-bucket.sh

--- .iac/create-bucket.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-: << 'comment'
-This script creates an Amazon S3 (Simple Storage Service) bucket
-comment
-
-aws s3api create-bucket --cli-input-json file://.iac/create-bucket.json --region "$1"
